52d665e97178fdf4ba590b80e07bce500c414b20
Jacky0729 <115882323+Jacky0729@users.noreply.github.com>
2024-04-16 14:47:01 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.c524cis0cir8.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "chongchunfai-employee"
+customregion = "us-east-1"
+
