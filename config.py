3183c943f23dcdca85f270be977a936c3e99c0ea
Jacky0729 <115882323+Jacky0729@users.noreply.github.com>
2024-04-16 15:03:43 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 customhost = "database-2.c524cis0cir8.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Aacs3253"
-customdb = "employee"
+customdb = "Employee"
 custombucket = "chongchunfai-employee"
 customregion = "us-east-1"
 
